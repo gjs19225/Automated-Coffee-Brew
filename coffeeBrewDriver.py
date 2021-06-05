74c82f6f9992bb8c563261a89ac4f9e87cb45196
Gabriel St Jean <gjs19225@uga.edu>
2021-06-05 18:50:42 -0400

    Created a more efficient square-wave drive

--- coffeeBrewDriver.py
+++ coffeeBrewDriver.py
@@ -8,7 +8,14 @@ import threading
 from threading import Thread
 
 # This program utilizes the digital output value of a Passive Infared Sensor (PIR)
-# to actuate the brewing of a coffee pot via two DC motors.
+# to actuate the brewing of a coffee pot via two DC motors. The two DC motors are
+# connected to a L293 H-bridge chip so that their directions can be reversed. The
+# two DC motors are connected together with a wire thread that tightens when
+# the motors are driven through a square-wave drive, allowing for the brewing button
+# on the coffee machine to be activated.
+#
+# Technical documentaion of the motion controlled coffee brewer can be found in the
+# README.md file of this repository.
 
 GPIO.setmode(GPIO.BCM)
 cannotDetectMotionLed = 15
@@ -24,12 +31,18 @@ GPIO.setup(canDetectMotionLed, GPIO.OUT)
 GPIO.setup(motionDetLed, GPIO.OUT)
 GPIO.setup(pirPin,GPIO.IN)
 GPIO.setup(resetButton, GPIO.IN)
-GPIO.setup(4, GPIO.OUT)  # DC motors
-GPIO.setup(5, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
+
+# DC motor 1
+GPIO.setup(5, GPIO.OUT) # Sends output signal to input port of H-bridge chip
 GPIO.setup(6, GPIO.OUT)
+GPIO.setup(4,GPIO.HIGH) # Enable port on H-bridge
+
+# DC motor 2
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
+GPIO.setup(21, GPIO.HIGH) # Enable port on H-bridge
+
 oldResetButtonValue = GPIO.input(resetButton)
 newResetButtonValue = GPIO.input(resetButton)
 
@@ -67,43 +80,54 @@ def motionDetection():
    while(canDetectMotion == True):
        # Detect motion until the sensor turns on
        if (GPIO.input(pirPin) == 1): # Motion detected
-          print("Motion detected")
           canDetectMotion = False
           GPIO.output(motionDetLed, GPIO.HIGH)
+          # Perform a square-wave drive with the DC motors
           for i in range(20):
-             GPIO.output(5,GPIO.LOW) # Perform a wave drive with the DC motors
-             GPIO.output(6,GPIO.HIGH)
-             GPIO.output(4,GPIO.HIGH)
+             GPIO.output(5, 0) # Move motor 1 clockwise
+             GPIO.output(6, 1)
+             GPIO.output(16, 1) # Move motor 2 counter-clockwise
+             GPIO.output(20, 0)
+             time.sleep(pulseWidth) # Sleep for 50 miliseconds
+             GPIO.output(5, 0) # Stop motor movement
+             GPIO.output(6, 0)
+             GPIO.output(16, 0)
+             GPIO.output(20, 0)
              time.sleep(pulseWidth)
-             GPIO.output(5,GPIO.HIGH)
-             GPIO.output(6,GPIO.HIGH)
-             GPIO.output(4,GPIO.HIGH)
+             GPIO.output(5, 0)
+             GPIO.output(6, 1)
+             GPIO.output(16, 1)
+             GPIO.output(20, 0)
              time.sleep(pulseWidth)
-             GPIO.output(5,GPIO.HIGH)
-             GPIO.output(6,GPIO.LOW)
-             GPIO.output(4, GPIO.HIGH)
+             GPIO.output(5, 0)
+             GPIO.output(6, 0)
+             GPIO.output(16, 0)
+             GPIO.output(20, 0)
              time.sleep(pulseWidth)
-             GPIO.output(5,GPIO.LOW)
-             GPIO.output(6,GPIO.LOW)
-             GPIO.output(4, GPIO.HIGH)
+          # Reverse the motors in case of any wire thread entanglement
+          for i in range(10):
+             # Perform a square-wave drive with the DC motors
+             GPIO.output(5, 1)
+             GPIO.output(6, 0)
+             GPIO.output(16, 0)
+             GPIO.output(20, 1)
              time.sleep(pulseWidth)
-             GPIO.output(16, GPIO.HIGH)
-             GPIO.output(20, GPIO.HIGH)
-             GPIO.output(21, GPIO.HIGH)
+             GPIO.output(5, 0)
+             GPIO.output(6, 0)
+             GPIO.output(16, 0)
+             GPIO.output(20, 0)
              time.sleep(pulseWidth)
-             GPIO.output(16, GPIO.HIGH)
-             GPIO.output(20, GPIO.HIGH)
-             GPIO.output(21, GPIO.HIGH)
+             GPIO.output(5, 1)
+             GPIO.output(6, 0)
+             GPIO.output(16, 0)
+             GPIO.output(20, 1)
              time.sleep(pulseWidth)
-             GPIO.output(16,GPIO.HIGH)
-             GPIO.output(20, GPIO.LOW)
-             GPIO.output(21, GPIO.HIGH)
+             GPIO.output(5, 0)
+             GPIO.output(6, 0)
+             GPIO.output(16, 0)
+             GPIO.output(20, 0)
              time.sleep(pulseWidth)
-             GPIO.output(16, GPIO.LOW)
-             GPIO.output(20, GPIO.LOW)
-             GPIO.output(21,GPIO.HIGH)
-             time.sleep(pulseWidth)
-             print(i)
+
 
 # Entry point to program
 GPIO.output(cannotDetectMotionLed, 0)
@@ -115,12 +139,10 @@ resetButtonThr.start()
 motionDetectThr = Thread(target=motionDetection, args=(), daemon=True)
 motionDetectThr.start()
 
-isMotionLoopStarted = False
 while True:
    if (not motionDetectThr.is_alive()):
       motionDetectThr = Thread(target=motionDetection, args=(), daemon=True)
       motionDetectThr.start()
-   print("count: ", count)
    time.sleep(1)
 
 # Indicate to the user that the program is no longer running
